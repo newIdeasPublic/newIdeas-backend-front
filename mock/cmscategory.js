29c2fb092e15d186fa63eeec7fd4298c723248f1
ivu4e <ivu4e@qq.com>
2020-12-19 17:34:13 +0800

    更新数据结构

--- mock/cmscategory.js
+++ mock/cmscategory.js
@@ -1,127 +1,133 @@
 const Mock = require('mockjs')
 
-const cates = [
-  {
-    'id': 1,
-    'createTime': '2020-12-08 01:05:59',
-    'updateTime': '2020-12-08 01:05:59',
-    'code': 'test',
-    'name': '分类测试001',
-    'description': '测试分类'
-  },
-  {
-    'id': 2,
-    'createTime': '2020-12-08 01:06:17',
-    'updateTime': '2020-12-08 01:06:17',
-    'code': 'test02',
-    'name': '分类测试002',
-    'description': '测试分类02'
-  },
-  {
-    'id': 3,
-    'createTime': '2020-12-14 13:37:16',
-    'updateTime': '2020-12-14 13:37:16',
-    'code': 'test03',
-    'name': '分类测试003',
-    'description': '测试分类03'
-  },
-  {
-    'id': 4,
-    'createTime': '2020-12-08 01:05:59',
-    'updateTime': '2020-12-08 01:05:59',
-    'code': 'test',
-    'name': '分类测试001',
-    'description': '测试分类'
-  },
-  {
-    'id': 5,
-    'createTime': '2020-12-08 01:06:17',
-    'updateTime': '2020-12-08 01:06:17',
-    'code': 'test02',
-    'name': '分类测试002',
-    'description': '测试分类02'
-  },
-  {
-    'id': 6,
-    'createTime': '2020-12-14 13:37:16',
-    'updateTime': '2020-12-14 13:37:16',
-    'code': 'test03',
-    'name': '分类测试003',
-    'description': '测试分类03'
-  },
-  {
-    'id': 7,
-    'createTime': '2020-12-08 01:05:59',
-    'updateTime': '2020-12-08 01:05:59',
-    'code': 'test',
-    'name': '分类测试001',
-    'description': '测试分类'
-  },
-  {
-    'id': 8,
-    'createTime': '2020-12-08 01:06:17',
-    'updateTime': '2020-12-08 01:06:17',
-    'code': 'test02',
-    'name': '分类测试002',
-    'description': '测试分类02'
-  },
-  {
-    'id': 9,
-    'createTime': '2020-12-14 13:37:16',
-    'updateTime': '2020-12-14 13:37:16',
-    'code': 'test03',
-    'name': '分类测试003',
-    'description': '测试分类03'
-  },
-  {
-    'id': 10,
-    'createTime': '2020-12-14 13:37:16',
-    'updateTime': '2020-12-14 13:37:16',
-    'code': 'test03',
-    'name': '分类测试003',
-    'description': '测试分类03'
-  },
-  {
-    'id': 11,
-    'createTime': '2020-12-08 01:05:59',
-    'updateTime': '2020-12-08 01:05:59',
-    'code': 'test',
-    'name': '分类测试001',
-    'description': '测试分类'
-  },
-  {
-    'id': 12,
-    'createTime': '2020-12-08 01:06:17',
-    'updateTime': '2020-12-08 01:06:17',
-    'code': 'test02',
-    'name': '分类测试002',
-    'description': '测试分类02'
-  },
-  {
-    'id': 13,
-    'createTime': '2020-12-14 13:37:16',
-    'updateTime': '2020-12-14 13:37:16',
-    'code': 'test03',
-    'name': '分类测试003',
-    'description': '测试分类03'
-  },
-  {
-    'id': 14,
-    'createTime': '2020-12-08 01:05:59',
-    'updateTime': '2020-12-08 01:05:59',
-    'code': 'test',
-    'name': '分类测试001',
-    'description': '测试分类'
-  },
-  {
-    'id': 15,
-    'createTime': '2020-12-08 01:06:17',
-    'updateTime': '2020-12-08 01:06:17',
-    'code': 'test02',
-    'name': '分类测试002',
-    'description': '测试分类02'
-  }
-]
+const data = {
+  'totalCount': 3,
+  'pageSize': 10,
+  'totalPage': 1,
+  'currPage': 0,
+  'list': [
+    {
+      'id': 1,
+      'createTime': '2020-12-08 01:05:59',
+      'updateTime': '2020-12-08 01:05:59',
+      'code': 'test',
+      'name': '分类测试001',
+      'description': '测试分类'
+    },
+    {
+      'id': 2,
+      'createTime': '2020-12-08 01:06:17',
+      'updateTime': '2020-12-08 01:06:17',
+      'code': 'test02',
+      'name': '分类测试002',
+      'description': '测试分类02'
+    },
+    {
+      'id': 3,
+      'createTime': '2020-12-14 13:37:16',
+      'updateTime': '2020-12-14 13:37:16',
+      'code': 'test03',
+      'name': '分类测试003',
+      'description': '测试分类03'
+    },
+    {
+      'id': 4,
+      'createTime': '2020-12-08 01:05:59',
+      'updateTime': '2020-12-08 01:05:59',
+      'code': 'test',
+      'name': '分类测试001',
+      'description': '测试分类'
+    },
+    {
+      'id': 5,
+      'createTime': '2020-12-08 01:06:17',
+      'updateTime': '2020-12-08 01:06:17',
+      'code': 'test02',
+      'name': '分类测试002',
+      'description': '测试分类02'
+    },
+    {
+      'id': 6,
+      'createTime': '2020-12-14 13:37:16',
+      'updateTime': '2020-12-14 13:37:16',
+      'code': 'test03',
+      'name': '分类测试003',
+      'description': '测试分类03'
+    },
+    {
+      'id': 7,
+      'createTime': '2020-12-08 01:05:59',
+      'updateTime': '2020-12-08 01:05:59',
+      'code': 'test',
+      'name': '分类测试001',
+      'description': '测试分类'
+    },
+    {
+      'id': 8,
+      'createTime': '2020-12-08 01:06:17',
+      'updateTime': '2020-12-08 01:06:17',
+      'code': 'test02',
+      'name': '分类测试002',
+      'description': '测试分类02'
+    },
+    {
+      'id': 9,
+      'createTime': '2020-12-14 13:37:16',
+      'updateTime': '2020-12-14 13:37:16',
+      'code': 'test03',
+      'name': '分类测试003',
+      'description': '测试分类03'
+    },
+    {
+      'id': 10,
+      'createTime': '2020-12-14 13:37:16',
+      'updateTime': '2020-12-14 13:37:16',
+      'code': 'test03',
+      'name': '分类测试003',
+      'description': '测试分类03'
+    },
+    {
+      'id': 11,
+      'createTime': '2020-12-08 01:05:59',
+      'updateTime': '2020-12-08 01:05:59',
+      'code': 'test',
+      'name': '分类测试001',
+      'description': '测试分类'
+    },
+    {
+      'id': 12,
+      'createTime': '2020-12-08 01:06:17',
+      'updateTime': '2020-12-08 01:06:17',
+      'code': 'test02',
+      'name': '分类测试002',
+      'description': '测试分类02'
+    },
+    {
+      'id': 13,
+      'createTime': '2020-12-14 13:37:16',
+      'updateTime': '2020-12-14 13:37:16',
+      'code': 'test03',
+      'name': '分类测试003',
+      'description': '测试分类03'
+    },
+    {
+      'id': 14,
+      'createTime': '2020-12-08 01:05:59',
+      'updateTime': '2020-12-08 01:05:59',
+      'code': 'test',
+      'name': '分类测试001',
+      'description': '测试分类'
+    },
+    {
+      'id': 15,
+      'createTime': '2020-12-08 01:06:17',
+      'updateTime': '2020-12-08 01:06:17',
+      'code': 'test02',
+      'name': '分类测试002',
+      'description': '测试分类02'
+    }
+  ]
+}
 
 module.exports = [
 
@@ -133,8 +139,8 @@ module.exports = [
       return {
         code: 20000,
         data: {
-          total: cates.length,
-          items: cates
+          total: data.totalCount,
+          items: data.list
         }
       }
     }
